abcc9b9fcd1a305a4df509ed54e2a4ef0d09cf04
TheStarhoyPro <75669181+TheStarhoyPro@users.noreply.github.com>
2020-12-11 21:51:46 +0530

    Add files via upload

---
+++ main(8).js
@@ -0,0 +1,68 @@
+function changeColor() {
+    document.getElementById("button1").style.color = "yellow"
+}
+
+function changeInitial() {
+    document.getElementById("button1").style.color = "blue"
+}
+
+function changeColor1() {
+    document.getElementById("button2").style.color = "yellow"
+}
+
+function changeInitial1() {
+    document.getElementById("button2").style.color = "blue"
+}
+
+function changeColor2() {
+    document.getElementById("button3").style.color = "yellow"
+}
+
+function changeInitial2() {
+    document.getElementById("button3").style.color = "blue"
+}
+
+function changeColor3() {
+    document.getElementById("button4").style.color = "yellow"
+}
+
+function changeInitial3() {
+    document.getElementById("button4").style.color = "blue"
+}
+
+
+function changeColor4() {
+    document.getElementById("button5").style.color = "yellow"
+}
+
+function changeInitial4() {
+    document.getElementById("button5").style.color = "blue"
+}
+
+function changeColor5() {
+    document.getElementById("button6").style.color = "yellow"
+}
+
+function changeInitial5() {
+    document.getElementById("button6").style.color = "blue"
+}
+
+function changeBgcolor() {
+    document.getElementById("h4").innerHTML = "About";
+    document.getElementById("h5").innerHTML = "Hi I am Arpit Kulhare and am 10 years old and I study in Lokhandwala Foundation school. I live in Mumbai. I am a WhitehatJr student and I have started to learn coding. Actually I started to learn coding when I was in first standard. At that time I started just basic scratch. I have completed 57 classes and 57 projects. I am on red hat and I have earned 11,550 points. I mostly code all the time other than studying books of my school. I love sports like basketball, cricket, etc. I also like cycling and reading books. And the most important thing, actually not that I love to play video games like call of duty, critical ops, etc. I play three games a day and I watch two youtube videos of my favourite and popular youtube channels Mr beast and Unspeakable.";
+}
+
+function changeBgcolor1() {
+    document.getElementById("h4").innerHTML = "My favourite website";
+    document.getElementById("h5").innerHTML = "My favourite websites contain websites like Google, Yahoo, Facebook, WhatsApp, and etc.";
+}
+
+function changeBgcolor2() {
+    document.getElementById("h4").innerHTML = "My work";
+    document.getElementById("h5").innerHTML = "This is my work link. Here you will get most of my projects> If you can follow me so please follow me too. The link = https://codepen.io/your-work";
+}
+
+function changeBgcolor3() {
+    document.getElementById("h4").innerHTML = "Contact";
+    document.getElementById("h5").innerHTML = "Email = arpitkulhare.thestarboy@gmail.com Phone Number = 9757157530 if any problem contact us.";
+}
